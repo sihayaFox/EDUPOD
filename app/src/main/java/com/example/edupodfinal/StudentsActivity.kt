3525f906703ff8bba9d77823debd0e952246e1f3
chanaka-AET <chanaka.r@aeturnum.com>
2021-09-20 15:45:00 +0530

    intilize project

---
+++ app/src/main/java/com/example/edupodfinal/StudentsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.edupodfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_students)
+    }
+}
